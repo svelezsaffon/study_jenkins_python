f104e8ced9cfb6497efa3ddb7c55cd2ab1c65a6d
svelezsaffon <svelezsaffon@gmail.com>
2022-12-13 11:04:32 -0500

    Add testing steps

---
+++ calculator/Calculcator.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def substract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
--- test/test_all.py
+++ test/test_all.py
@@ -1,6 +1,23 @@
 import unittest
+from calculator.Calculcator import Calculator
 
 
+class TestCalculator(unittest.TestCase):
 
-def test_all_():
-    assert True
+    def test_substract(self):
+        """
+        Test substract on int 4-2=2
+        :return:
+        """
+        self.assertEqual(Calculator().substract(4, 2), 2)
+
+    def test_add(self):
+        """
+        Tests add on int 4+2=6
+        :return:
+        """
+        self.assertEqual(Calculator().add(4, 2), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
